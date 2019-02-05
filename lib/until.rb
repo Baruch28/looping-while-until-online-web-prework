1b5bedf3865815ee40cc4ac499620fe96fcdb0d4
mark baruch <mbaruch28@gmail.com>
2019-02-05 14:45:27 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,6 @@
 def using_until
 levitation_force = 6
 until levitation = 10
-  puts "Wingardium Leviosa is less than 10"          
-  levitation_force += 1  
+  puts "Wingardium Leviosa is less than 10"
+  levitation_force += 1
 end
-
